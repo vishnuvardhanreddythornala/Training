fa73673949dd73c76f6350b3ae79aa1564532d39
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2025-12-29 09:56:48 +0530

    Added practice problems

---
+++ src/module9/recursion/explicitcasting.java
@@ -0,0 +1,14 @@
+//wajp to assign an int value 130 to a byte using explicit casting and print the output
+package module9.recursion;
+
+public class explicitcasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 130;
+		byte b = (byte)i;
+		System.out.println(b);
+
+	}
+
+}
---
+++ src/module9/recursion/narrowingdoubletoint.java
@@ -0,0 +1,14 @@
+//wajp to convert a double value 45.78 into an int and print result.
+package module9.recursion;
+
+public class narrowingdoubletoint {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		double a = 45.78;
+		int b = (int)a;
+		System.out.println(b);
+	}
+
+}
