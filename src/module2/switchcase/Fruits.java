06015eb36929ed9cee3bad58b36bfe33257871d4
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-01-07 11:13:58 +0530

    Applications of Switch Case

---
+++ src/module2/switchcase/Fruits.java
@@ -0,0 +1,20 @@
+/*
+ * create a program it takes input as a fruits and print corresponding statements
+    "apple" : Apples are rich in fiber and vitamin c
+    "banana" : bananas are the great source of potassium
+    "orange" : oranges are packed with vitamin c
+    "mango" : Mangoes are the king of fruits!
+    "Others" : Sorry, I don't have information about that fruit
+ */
+package module2.switchcase;
+import java.util.Scanner;
+
+public class Fruits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
